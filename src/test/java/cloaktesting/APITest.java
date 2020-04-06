e53ebc3b5aa176ff92daece1b49d49f353f8c65b
daniel <deathcloak@gmail.com>
2020-04-06 16:01:02 -0400

    American guy commit

--- src/test/java/cloaktesting/APITest.java
+++ src/test/java/cloaktesting/APITest.java
@@ -10,6 +10,10 @@ public class APITest {
 		System.out.println("Post Jira");
 		System.out.println("Post Jira2");
 		System.out.println("Post Jira3");
+		System.out.println("Post Jira4");
+		System.out.println("Post Jira5");
+		System.out.println("Post Jira6");
+		System.out.println("Post Jira7");
 	}
 	
 	@Test 
--- src/test/java/cloaktesting/SeleniumTest.java
+++ src/test/java/cloaktesting/SeleniumTest.java
@@ -9,6 +9,7 @@ public class SeleniumTest {
 	@Test
 	public void browserAutomation() {
 		System.out.println("Browser Automation");
+		System.out.println("Web Automation");
 	}
 	
 	@Test
